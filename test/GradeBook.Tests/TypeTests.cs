f50f08d238f7da88a92b6195fe1862c8f28a1327
Jake Myhill <squiibles@gmail.com>
2020-01-08 14:12:29 +0000

    First commit

---
+++ test/GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,124 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class TypeTests
+    {   
+        int count = 0;
+        public delegate string WriteLogDelegate(string logMessage);
+
+
+        [Fact]
+        public void WriteLogDelegateCanPointToMethod()
+        {
+
+            WriteLogDelegate log = ReturnMessage;
+
+            log += ReturnMessage;
+            log += IncrementCount;
+
+            var result = log("Hello!");
+            Assert.Equal(3, count);
+        }
+
+        string IncrementCount(string message)
+        {
+            count++;
+            return message.ToLower;
+        }
+
+        string ReturnMessage(string message)
+        {
+            count++;
+            return message;
+        }
+
+        [Fact]
+        public void ValueTypesAlsoPassByValue()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+
+        }
+        private void GetBookSetName(ref InMemoryBook book, string name)
+        {
+            book = new InMemoryBook(name);
+        }
+
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+
+        }
+        private void GetBookSetName(InMemoryBook book, string name)
+        {
+            book = new InMemoryBook(name);
+        }
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Jake";
+            var upper = MakeUpperCase(name);
+
+            Assert.Equal("Jake", name);
+            Assert.Equal("JAKE", upper);
+        }
+
+        private string MakeUpperCase(string parameter)
+        {
+           return parameter.ToUpper();
+        }
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObjects()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
+        }
+        InMemoryBook GetBook(string name)
+        {
+            return new InMemoryBook(name);
+        }
+    }
+}
